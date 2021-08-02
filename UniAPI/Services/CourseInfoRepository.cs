67d94ceb42853e28dd45d03960c87ffc265ca42f
Marwan Al Khasawneh <marwankhasawneh@gmail.com>
2021-08-02 15:13:51 +0300

    Created Repository Interface

---
+++ UniAPI/Services/CourseInfoRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UniAPI.Contexts;
+using UniAPI.Entities;
+
+namespace UniAPI.Services
+{
+    public class CourseInfoRepository : ICourseInfoRepository
+    {
+        private readonly CourseInfoContext _context;
+
+        public CourseInfoRepository(CourseInfoContext context)
+        {
+            _context = context ?? throw new ArgumentNullException();
+        }
+        public IEnumerable<Course> GetAllCourse()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Course GetCourseById(int courseId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Course> GetCoursesByStudent(int studentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student GetStudentById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Student> GetStudentsByCourse(int courseId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UniAPI/Services/ICourseInfoRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UniAPI.Entities;
+
+namespace UniAPI.Services
+{
+    public interface ICourseInfoRepository
+    {
+        IEnumerable<Course> GetAllCourse();
+
+        Course GetCourseById(int courseId);
+
+        IEnumerable<Course> GetCoursesByStudent(int studentId);
+
+        IEnumerable<Student> GetAllStudents();
+
+        Student GetStudentById();
+
+        IEnumerable<Student> GetStudentsByCourse(int courseId);
+    }
+}
