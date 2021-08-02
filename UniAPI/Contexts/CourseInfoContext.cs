744e7e6115a787176b7850f9026649212136e318
Marwan Al Khasawneh <marwankhasawneh@gmail.com>
2021-08-02 14:45:33 +0300

    Added a database context

---
+++ UniAPI/Contexts/CourseInfoContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using UniAPI.Entities;
+
+namespace UniAPI.Contexts
+{
+    public class CourseInfoContext : DbContext
+    {
+        public DbSet<Course> Courses { get; set; }
+
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<Lecturer> Lecturers { get; set; }
+
+        public DbSet<ClassRoom> ClassRooms { get; set; }
+
+        public CourseInfoContext(DbContextOptions<CourseInfoContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+}
