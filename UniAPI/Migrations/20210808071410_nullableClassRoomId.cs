8783d7bb776673a6751511655db5601afc818439
Marwan Al Khasawneh <marwankhasawneh@gmail.com>
2021-08-08 10:17:22 +0300

    Added the ability of having a course without a classroom

---
+++ UniAPI/Migrations/20210808071410_nullableClassRoomId.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UniAPI.Migrations
+{
+    public partial class nullableClassRoomId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_ClassRooms_ClassRoomId",
+                table: "Courses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ClassRoomId",
+                table: "Courses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_ClassRooms_ClassRoomId",
+                table: "Courses",
+                column: "ClassRoomId",
+                principalTable: "ClassRooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_ClassRooms_ClassRoomId",
+                table: "Courses");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ClassRoomId",
+                table: "Courses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_ClassRooms_ClassRoomId",
+                table: "Courses",
+                column: "ClassRoomId",
+                principalTable: "ClassRooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
